b6f1e302b7f161a96e92b9978788e29cb1879986
Hakisalehyoussouf <hakisalehyoussouf14@gmail.com>
2023-06-03 04:09:38 +0100

    Comments

--- src/main/java/ma/ensate/Test.java
+++ src/main/java/ma/ensate/Test.java
@@ -1,9 +1,12 @@
+/**
+ * Comments!!!!!!!!!!!!!!!!!!!!!
+ */
 package ma.ensate;
-
-
 public class Test{
 
     public static void main(String[] args){
+
+        //___________Just Comments__________________________
         System.out.println("Hello World!");
     }
 }
